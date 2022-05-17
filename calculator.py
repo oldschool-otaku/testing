c01923ea92523172f62e1cc1efe0914b8a0fea89
denamo <90336027+oldschool-otaku@users.noreply.github.com>
2022-05-17 18:10:35 +0300

    кал и тка

---
+++ calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def sum(x, y):
+        return x + y
+    
+    def div(x, y):
+        if y == 0:
+            return "НА 0 ДЕЛИТЬ НЕЛЬЗЯ"
+        else : return x / y
+
+    def sub(x, y):
+        return x - y
+    
+    def mul(x, y):
+        return x * y
+
+    def pwr(x):
+        return x * x
